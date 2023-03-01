d4516c0ff3cba28fdd2e3c5a94993732b2c4e0d3
NickWebDev007 <126471422+NickWebDev007@users.noreply.github.com>
2023-03-01 20:26:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const theList = [
+  'Laurence', 'Svekis', true, 35, null, undefined,
+  {test:'one',score:55},
+  ['one','two']
+  ]
+ theList.pop()
+ theList.pop()
+ theList.pop()
+ theList.pop()
+ theList.shift()
+ theList.push('lAST')
+ theList[2]= 'Hello world'
+ theList.unshift('FIRST')
+ 
+ theList[2]= 'MIDDLE'
+ 
+
+ console.log(theList);
+ //console.log(theList);
+ 
+ 
